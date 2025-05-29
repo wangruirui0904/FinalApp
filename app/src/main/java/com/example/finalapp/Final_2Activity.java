7fd0a63a1ccb70ceca50fe9a1ddacefad2a5b663
wangruirui0904 <1129442527@qq.com>
2025-05-29 15:57:15 +0800

    second page.xml  and  background

--- app/src/main/java/com/example/finalapp/Final_2Activity.java
+++ app/src/main/java/com/example/finalapp/Final_2Activity.java
@@ -1,6 +1,8 @@
 package com.example.finalapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +15,29 @@ public class Final_2Activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_final2);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        Button button2 = findViewById(R.id.button_2);
+        button2.setOnClickListener(v -> {
+            // 创建 Intent 跳转到 Final_2Activity
+            Intent intent = new Intent(Final_2Activity.this, Final_2_1Activity.class);
+            startActivity(intent);
         });
+        // 初始化按钮3并设置跳转到Final_2_2Activity
+        Button button3 = findViewById(R.id.button_3);
+        button3.setOnClickListener(v -> {
+            Intent intent = new Intent(Final_2Activity.this, Final_2_2Activity.class);
+            startActivity(intent);
+        });
+        Button button4 = findViewById(R.id.button_4);
+        button4.setOnClickListener(v -> {
+            Intent intent = new Intent(Final_2Activity.this, Final_2_3Activity.class);
+            startActivity(intent);
+        });
+        Button button5 = findViewById(R.id.button_5);
+        button5.setOnClickListener(v -> {
+            Intent intent = new Intent(Final_2Activity.this, Final_2_4Activity.class);
+            startActivity(intent);
+        });
+
     }
 }
