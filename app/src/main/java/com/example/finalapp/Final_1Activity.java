e14163221ea8860a26351295257dad70fe507266
wangruirui0904 <1129442527@qq.com>
2025-05-29 15:26:41 +0800

    first page.java and second page.xml

--- app/src/main/java/com/example/finalapp/Final_1Activity.java
+++ app/src/main/java/com/example/finalapp/Final_1Activity.java
@@ -1,6 +1,9 @@
 package com.example.finalapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,13 @@ public class Final_1Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_final1);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        Button btn_enter = findViewById(R.id.btn_enter);
+        btn_enter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View btn) {
+                Intent intent = new Intent(Final_1Activity.this, Final_2Activity.class);
+                startActivity(intent);
+            }
         });
     }
 }
